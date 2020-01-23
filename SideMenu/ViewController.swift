98be76ccdf43c6e6c0060fb1156ce69dd2cbe887
Ramy <rnjtbvhbv>
2020-01-23 03:04:08 +0200

    Initial Commit

---
+++ SideMenu/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by Reimo on 11/21/19.
+//  Copyright © 2019 Reimo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var SideMenuLayout: NSLayoutConstraint!
+    
+    @IBAction func SideMenuButton(_ sender: UIButton) {
+     /*   if SideMenuLayout.constant == 0 {
+            SideMenuLayout.constant = self.view.frame.width * 0.7}
+        else{SideMenuLayout.constant = 0}
+        */
+        
+        SideMenuLayout.constant = SideMenuLayout.constant == 0 ? self.view.frame.width * 0.7 : 0
+        
+        UIView.animate(withDuration: 0.5) {
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+    
+    
+
+
+}
+
